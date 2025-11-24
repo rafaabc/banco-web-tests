6cb9c2cc764fa9954012bf62ac58b8c44890b8af
rafaabc <faelsabc21@gmail.com>
2025-11-24 17:53:14 +0100

    add fixtures to avoid hardcoded data and duplication

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,15 +3,20 @@ describe("Login", () => {
     cy.visit("http://localhost:4000/");
   });
   it("When using valid credentials to login, then show the secret page", () => {
-    cy.get("#username").click().type("julio.lima");
-    cy.get("#senha").click().type("123456");
+    cy.fixture("credentials").then((credentials) => {
+      cy.get("#username").click().type(credentials.valid.username);
+      cy.get("#senha").click().type(credentials.valid.senha);
+    });
     cy.contains("button", "Entrar").click();
     cy.contains("Realizar Transferência").should("be.visible");
   });
 
   it("When using invalid credentials to login, then show a toast message", () => {
-    cy.get("#username").click().type("julio.limas");
-    cy.get("#senha").click().type("123456");
+    cy.fixture("credentials").then((credentials) => {
+      cy.get("#username").click().type(credentials.invalid.username);
+      cy.get("#senha").click().type(credentials.invalid.senha);
+    });
+
     cy.contains("button", "Entrar").click();
     cy.get(".toast").should("have.text", "Erro no login. Tente novamente.");
   });
